10f12817f4e10fe405e8abd21decdea078d01092
Jonathan Jimenez <jonathanjimenez@heliosphere.attlocal.net>
2017-01-02 19:58:23 -0800

    Initial Commit. Set up directory structure, included links, added test file

---
+++ README.md
@@ -0,0 +1,14 @@
+#Structure
+Solutions have the following characteristics
+- Single file
+- Named `main.go`
+- directory should contain `in` and `out` file
+- `in` and `out` file should be exactly the same as samples provided by icpc
+- Each year has it's own directory, each problem has it's own directory
+- In the root directory there is a main_test.go file that works for all tests
+- In the root directory there is a main.go file with boilerplate
+#Testing
+Tests are provided in the form of golang tests
+
+#Contributing
+This repo isn't meant to be an authoritative set of solution, it's just to keep my work organized. However, if you find a problem with any solutions, please feel free to let me know how it could have been done better.
---
+++ main_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"os"
+	"testing"
+)
+
+func TestSolution(t *testing.T) {
+	// Prepare input and output
+	inFile, err := os.Open("in")
+	if err != nil {
+		t.Error(err)
+	}
+	defer inFile.Close()
+	resultBuffer := new(bytes.Buffer)
+	reader := bufio.NewReader(inFile)
+	writer := bufio.NewWriter(resultBuffer)
+
+	// Solve Problem
+	Solution(reader, writer)
+
+	// Prepare Buffers for testing
+	desiredFile, err := os.Open("out")
+	if err != nil {
+		t.Error(err)
+	}
+	defer desiredFile.Close()
+	desiredScanner := bufio.NewScanner(desiredFile)
+	resultScanner := bufio.NewScanner(resultBuffer)
+
+	// Compare output
+	for {
+		moreDesired := desiredScanner.Scan()
+		moreResult := resultScanner.Scan()
+		if moreDesired || moreResult {
+			if desiredScanner.Text() != resultScanner.Text() {
+				t.Errorf("%s VS %s", desiredScanner.Text(), resultScanner.Text())
+			}
+		} else {
+			break
+		}
+	}
+
+}
